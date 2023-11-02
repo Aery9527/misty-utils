80e562760051ed105ac6c9e24c41d156c88a2845
aerylin <aerylin@pactmail.com>
2023-11-02 11:23:24 +0800

    change class name from Judge to Checker

--- src/main/java/org/misty/utils/ex/verify/Checker.java
+++ src/main/java/org/misty/utils/ex/verify/Checker.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
-public class Judge {
+public class Checker {
 
     @SuppressWarnings("rawtypes")
     public static boolean isNullOrEmpty(Object o) {

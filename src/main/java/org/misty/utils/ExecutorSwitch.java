eb6f4b32dd9785be5d1f067ace804f812335a6ca
aerylin <aerylin@pactmail.com>
2023-11-02 11:29:32 +0800

    add ExecutorSwitch

---
+++ src/main/java/org/misty/utils/ExecutorSwitch.java
@@ -0,0 +1,290 @@
+package org.misty.utils;
+
+import org.misty.utils.ex.fi.IntConsumerEx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiPredicate;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class ExecutorSwitch {
+
+    public interface Action {
+
+        void execute(int times) throws Exception;
+
+        boolean errorHandler(int times, Exception e);
+
+    }
+
+    public static ExecutorSwitch create() {
+        return new ExecutorSwitch(Tracked.create());
+    }
+
+    public static ExecutorSwitch create(BiPredicate<Integer, Exception> defaultErrorHandler) {
+        return new ExecutorSwitch(Tracked.create(), defaultErrorHandler);
+    }
+
+    public static ExecutorSwitch create(String name) {
+        return new ExecutorSwitch(Tracked.create(name));
+    }
+
+    public static ExecutorSwitch create(String name, BiPredicate<Integer, Exception> defaultErrorHandler) {
+        return new ExecutorSwitch(Tracked.create(name), defaultErrorHandler);
+    }
+
+    public static ExecutorSwitch create(Tracked tracked) {
+        return new ExecutorSwitch(tracked);
+    }
+
+    public static ExecutorSwitch create(Tracked tracked, BiPredicate<Integer, Exception> defaultErrorHandler) {
+        return new ExecutorSwitch(tracked, defaultErrorHandler);
+    }
+
+    /**
+     * 用來表達錯誤處理器的回傳值, 表示繼續執行
+     */
+    public static final boolean WHEN_ERROR_CONTINUE = false;
+
+    /**
+     * 同{@link #WHEN_ERROR_CONTINUE}, 表示中斷執行
+     */
+    public static final boolean WHEN_ERROR_BREAK = true;
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 啟動鎖, 當第一次執行{@link #run(IntConsumerEx, BiPredicate)}開始就不得改變執行模式({@link #withParallel(ExecutorService)}或{@link #withSerial()})
+     */
+    private final AtomicBoolean startLock = new AtomicBoolean(false);
+
+    /**
+     * 中斷鎖, 當任務發生錯誤時, 這邊會被設定為true, 表示中斷執行
+     */
+    private final AtomicBoolean breakFlag = new AtomicBoolean(false);
+
+    /**
+     * 執行計數器, 當任務被執行時, 這邊會被加1
+     */
+    private final AtomicInteger executeCount = new AtomicInteger(0);
+
+    /**
+     * 追蹤碼, 方便用來追蹤哪個{@link ExecutorSwitch}實例執行錯誤
+     */
+    private final Tracked tracked;
+
+    /**
+     * 預設的錯誤處理器
+     */
+    private final BiPredicate<Integer, Exception> defaultErrorHandler;
+
+    /**
+     * 任務執行動作, 會根據{@link #withParallel(ExecutorService)}或{@link #withSerial()}來決定是同步或非同步執行
+     */
+    private Consumer<Runnable> runAction;
+
+    /**
+     * 任務執行完成檢查動作, 用於主thread等待使用
+     */
+    private Predicate<Duration> finishCheckAction;
+
+    private boolean withSerial;
+
+    public ExecutorSwitch(Tracked tracked) {
+        this.tracked = tracked;
+        this.defaultErrorHandler = ((times, e) -> {
+            this.logger.error(this.tracked.say("error when times({})"), times, e);
+            return WHEN_ERROR_CONTINUE;
+        });
+
+        withSerial();
+        reset();
+    }
+
+    public ExecutorSwitch(Tracked tracked, BiPredicate<Integer, Exception> defaultErrorHandler) {
+        this.tracked = tracked;
+        this.defaultErrorHandler = defaultErrorHandler;
+
+        withSerial();
+        reset();
+    }
+
+    /**
+     * {@link #withParallel(ExecutorService)}, threadNumber自動使用CPU核心數
+     */
+    public ExecutorSwitch withParallel() {
+        int availableCores = Runtime.getRuntime().availableProcessors();
+        return withParallel(availableCores);
+    }
+
+    /**
+     * {@link #withParallel(ExecutorService)}
+     */
+    public ExecutorSwitch withParallel(int threadNumber) {
+        withParallel(Executors.newFixedThreadPool(threadNumber));
+        return this;
+    }
+
+    /**
+     * 使用多執行緒模式
+     */
+    public ExecutorSwitch withParallel(ExecutorService executorService) {
+        checkStartLock();
+
+        AtomicInteger finishCount = new AtomicInteger(0);
+        Object lock = new Object();
+
+        BooleanSupplier checkFinish = () -> finishCount.get() == this.executeCount.get();
+
+        this.runAction = action -> executorService.execute(() -> {
+            try {
+                action.run();
+            } finally {
+                finishCount.incrementAndGet();
+
+                synchronized (lock) {
+                    lock.notifyAll();
+                }
+            }
+        });
+
+        this.finishCheckAction = (waitTime) -> {
+            long waitMs = waitTime.toMillis();
+
+            Watcher watcher = Watcher.create();
+            while (!watcher.over(waitMs)) {
+                long through = watcher.through();
+                long timeout = Math.min(1000, waitMs - through);
+
+                try {
+                    synchronized (lock) {
+                        if (checkFinish.getAsBoolean()) {
+                            return true;
+                        }
+
+                        lock.wait(timeout);
+
+                        if (checkFinish.getAsBoolean()) {
+                            return true;
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // reset interrupt flag
+                    this.logger.error(this.tracked.say("wait job"), e);
+                    return checkFinish.getAsBoolean();
+                }
+            }
+
+            return checkFinish.getAsBoolean();
+        };
+
+        this.withSerial = false;
+
+        return this;
+    }
+
+    /**
+     * 使用單線程模式
+     */
+    public ExecutorSwitch withSerial() {
+        checkStartLock();
+
+        this.runAction = Runnable::run;
+        this.finishCheckAction = waitTime -> true;
+        this.withSerial = true;
+        return this;
+    }
+
+    private void checkStartLock() {
+        if (this.startLock.get()) {
+            throw new IllegalStateException("please reset first before change mode.");
+        }
+    }
+
+    /**
+     * 會無限等待之前已送出的任務全部完成, 才會進行rest
+     */
+    public ExecutorSwitch reset() {
+        waitFinish();
+
+        this.startLock.set(false);
+        this.breakFlag.set(false);
+        this.executeCount.set(0);
+        return this;
+    }
+
+    /**
+     * 參考{@link #run(IntConsumerEx, BiPredicate)}
+     */
+    public boolean run(Action executeAction) {
+        return run(executeAction::execute, executeAction::errorHandler);
+    }
+
+    /**
+     * 參考{@link #run(IntConsumerEx, BiPredicate)}
+     */
+    public boolean run(IntConsumerEx executeAction) {
+        return run(executeAction, this.defaultErrorHandler);
+    }
+
+    /**
+     * 同步或非同步執行executeAction
+     *
+     * @param executeAction 欲工作的任務內容
+     * @param errorHandler  錯誤處理器, 回傳true表示中斷執行, 回傳false表示繼續執行, 參考{@link ExecutorSwitch#WHEN_ERROR_CONTINUE}與{@link ExecutorSwitch#WHEN_ERROR_BREAK}
+     * @return true表示該任務有執行, false表示任務沒有執行
+     */
+    public boolean run(IntConsumerEx executeAction, BiPredicate<Integer, Exception> errorHandler) {
+        this.startLock.set(true);
+
+        if (this.breakFlag.get()) {
+            return false;
+        }
+
+        int times = this.executeCount.incrementAndGet();
+        this.runAction.accept(() -> {
+            try {
+                executeAction.handle(times);
+            } catch (Exception e) {
+                boolean breakFlag = true;
+                try {
+                    breakFlag = errorHandler.test(times, e);
+                } catch (Exception ex) {
+                    this.logger.error(this.tracked.say("raw error when times({})"), times, e);
+                    this.logger.error(this.tracked.say("errorHandler error when times({})"), times, ex);
+                }
+                this.breakFlag.compareAndSet(false, breakFlag);
+            }
+        });
+
+        return true;
+    }
+
+    public void waitFinish() {
+        waitFinish(Duration.ofMillis(Long.MAX_VALUE));
+    }
+
+    public boolean waitFinish(Duration waitTime) {
+        return this.finishCheckAction.test(waitTime);
+    }
+
+    public Tracked getTracked() {
+        return tracked;
+    }
+
+    public boolean isWithSerial() {
+        return this.withSerial;
+    }
+
+    public boolean isWithParallel() {
+        return !this.withSerial;
+    }
+
+}
---
+++ src/test/java/org/misty/utils/ExecutorSwitchTest.java
@@ -0,0 +1,217 @@
+package org.misty.utils;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.misty._utils.AssertionsEx;
+import org.misty.utils.ex.ThreadEx;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiPredicate;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
+
+class ExecutorSwitchTest {
+
+    @Test
+    public void create() {
+        {
+            String name = "kerker";
+            ExecutorSwitch executorSwitch = ExecutorSwitch.create(name);
+            Tracked tracked = executorSwitch.getTracked();
+            Assertions.assertThat(tracked.getId().startsWith(name)).isTrue();
+        }
+
+        {
+            Tracked tracked = Tracked.create();
+            ExecutorSwitch executorSwitch = ExecutorSwitch.create(tracked);
+            Assertions.assertThat(executorSwitch.getTracked()).isEqualTo(tracked);
+        }
+    }
+
+    @Test
+    public void create_errorHandler() {
+        AtomicInteger counter1 = new AtomicInteger(0);
+        BiPredicate<Integer, Exception> defaultErrorHandler = (times, error) -> {
+            counter1.incrementAndGet();
+            return ExecutorSwitch.WHEN_ERROR_CONTINUE;
+        };
+
+        AtomicInteger counter2 = new AtomicInteger(0);
+        BiPredicate<Integer, Exception> currentErrorHandler = (times, error) -> {
+            counter2.incrementAndGet();
+            return ExecutorSwitch.WHEN_ERROR_CONTINUE;
+        };
+
+        ExecutorSwitch executorSwitch1 = ExecutorSwitch.create("kerker", defaultErrorHandler);
+        ExecutorSwitch executorSwitch2 = ExecutorSwitch.create(Tracked.create(), defaultErrorHandler);
+
+        executorSwitch1.run(times -> {
+            throw new RuntimeException();
+        });
+        Assertions.assertThat(counter1.get()).isEqualTo(1);
+        executorSwitch2.run(times -> {
+            throw new RuntimeException();
+        });
+        Assertions.assertThat(counter1.get()).isEqualTo(2);
+
+        executorSwitch1.run(times -> {
+            throw new RuntimeException();
+        }, currentErrorHandler);
+        Assertions.assertThat(counter2.get()).isEqualTo(1);
+        executorSwitch2.run(times -> {
+            throw new RuntimeException();
+        }, currentErrorHandler);
+        Assertions.assertThat(counter2.get()).isEqualTo(2);
+    }
+
+    @Test
+    public void run_withSerial() {
+        ExecutorSwitch executorSwitch = ExecutorSwitch.create(); // default is withSerial
+
+        AtomicReference<Thread> threadRef = new AtomicReference<>();
+        executorSwitch.run(times -> threadRef.set(Thread.currentThread()));
+
+        Assertions.assertThat(threadRef.get()).isEqualTo(Thread.currentThread());
+    }
+
+    @Test
+    public void run_withParallel() {
+        int threadNumber = 5;
+        ExecutorSwitch executorSwitch = ExecutorSwitch.create().withParallel(threadNumber);
+
+        List<Thread> threads = new ArrayList<>(threadNumber);
+        IntStream.range(0, threadNumber).forEach(index -> {
+            executorSwitch.run(times -> threads.add(Thread.currentThread()));
+        });
+
+        executorSwitch.waitFinish(); // 這裡要等待fork出去的thread都執行完再往下
+
+        Assertions.assertThat(threads).doesNotContain(Thread.currentThread());
+    }
+
+    @Test
+    public void run_error_do_continue_or_break_withSerial() {
+        int stopTimes = 3;
+
+        AtomicInteger errorCounter = new AtomicInteger(0);
+        ExecutorSwitch executorSwitch = ExecutorSwitch.create((times, e) -> { // defaultErrorHandler
+            int count = errorCounter.incrementAndGet();
+            return count == stopTimes ? ExecutorSwitch.WHEN_ERROR_BREAK : ExecutorSwitch.WHEN_ERROR_CONTINUE;
+        }).withSerial();
+
+        BooleanSupplier test = () -> executorSwitch.run(times -> {
+            throw new RuntimeException();
+        });
+
+        Assertions.assertThat(test.getAsBoolean()).isTrue();
+        Assertions.assertThat(test.getAsBoolean()).isTrue();
+        Assertions.assertThat(test.getAsBoolean()).isTrue();
+        Assertions.assertThat(test.getAsBoolean()).isFalse();
+        Assertions.assertThat(test.getAsBoolean()).isFalse();
+        Assertions.assertThat(test.getAsBoolean()).isFalse();
+    }
+
+    @Test
+    public void run_error_do_continue_or_break_withParallel() {
+        int stopTimes = 3;
+        int threadNumber = 5;
+
+        AtomicInteger errorCounter = new AtomicInteger(0);
+        ExecutorSwitch executorSwitch = ExecutorSwitch.create((times, e) -> { // defaultErrorHandler
+            int count = errorCounter.incrementAndGet();
+            return count >= stopTimes ? ExecutorSwitch.WHEN_ERROR_BREAK : ExecutorSwitch.WHEN_ERROR_CONTINUE;
+        }).withParallel(threadNumber);
+
+        Map<Boolean, AtomicInteger> resultCounter = new HashMap<>();
+        for (int i = 0; i < threadNumber; i++) {
+            boolean executed = executorSwitch.run(times -> {
+                throw new RuntimeException();
+            });
+            resultCounter.computeIfAbsent(executed, key -> new AtomicInteger(0)).incrementAndGet();
+            ThreadEx.rest(100);
+        }
+
+        executorSwitch.waitFinish(); // 這裡要等待fork出去的thread都執行完再往下
+
+        Assertions.assertThat(errorCounter.get()).isEqualTo(stopTimes);
+        Assertions.assertThat(resultCounter.get(Boolean.TRUE).get()).isEqualTo(stopTimes);
+        Assertions.assertThat(resultCounter.get(Boolean.FALSE).get()).isEqualTo(threadNumber - stopTimes);
+    }
+
+    @Test
+    public void startLock_and_reset() {
+        ExecutorSwitch executorSwitch = ExecutorSwitch.create();
+        executorSwitch.run(times -> ThreadEx.rest(10));
+
+        AssertionsEx.assertThrown(executorSwitch::withSerial).isInstanceOf(IllegalStateException.class);
+        AssertionsEx.assertThrown(() -> executorSwitch.withParallel(1)).isInstanceOf(IllegalStateException.class);
+
+        executorSwitch.reset();
+
+        executorSwitch.withParallel();
+        executorSwitch.withSerial();
+    }
+
+    @Test
+    public void waitFinish() {
+        long restLeast = 50;
+        long restMost = 100;
+        long restLeast_2 = restLeast / 2;
+
+        Consumer<ExecutorSwitch> runRest = executorSwitch -> executorSwitch.run(times -> ThreadEx.restRandom(restLeast, restMost));
+
+        Consumer<ExecutorSwitch> test = executorSwitch -> {
+            boolean isWithSerial = executorSwitch.isWithSerial();
+
+            Watcher watcher = Watcher.create();
+            runRest.accept(executorSwitch);
+            runRest.accept(executorSwitch);
+            runRest.accept(executorSwitch);
+            long through1 = watcher.through();
+
+            if (isWithSerial) {
+                Assertions.assertThat(through1).isGreaterThan(restLeast * 3); // 單緒是先完成任務才會往後跑
+            } else {
+                Assertions.assertThat(through1).isLessThanOrEqualTo(10); // 多緒這邊不會花太多時間
+            }
+
+            watcher.reset();
+            boolean finished1 = executorSwitch.waitFinish(Duration.ofMillis(restLeast_2));
+            long through2 = watcher.through();
+
+            if (isWithSerial) {
+                Assertions.assertThat(finished1).isTrue(); // 單緒是先完成任務才會往後跑
+                Assertions.assertThat(through2).isLessThanOrEqualTo(10); // 單緒這邊並不會有等待時間
+            } else {
+                Assertions.assertThat(finished1).isFalse(); // 多緒任務還在執行未完成
+                Assertions.assertThat(through2).isCloseTo(restLeast_2, Offset.offset(200L)); // 所以多緒會有等待時間 (這裡有時候會超時耶...)
+            }
+
+            watcher.reset();
+            boolean finished2 = executorSwitch.waitFinish(Duration.ofMillis(restMost));
+            long through3 = watcher.through();
+
+            Assertions.assertThat(finished2).isTrue();
+            if (isWithSerial) {
+                Assertions.assertThat(through3).isLessThanOrEqualTo(10); // 單緒這邊並不會有等待時間
+            } else {
+                Assertions.assertThat(through3).isGreaterThan(restLeast - (restLeast_2 + 20)); // 多緒前面等了restLeast_2, 所以這邊會比restLeast少
+                Assertions.assertThat(through3).isLessThan(restMost - restLeast_2 + 20); // +20是因為waitFinish的實作有可能會多等一點點
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            test.accept(ExecutorSwitch.create().withSerial());
+            test.accept(ExecutorSwitch.create().withParallel());
+        }
+    }
+
+}

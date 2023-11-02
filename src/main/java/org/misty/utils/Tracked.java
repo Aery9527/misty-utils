f897a68ad87297b54362bfb69620137a38d62741
aerylin <aerylin@pactmail.com>
2023-11-02 11:27:43 +0800

    add Tracked

---
+++ src/main/java/org/misty/utils/Tracked.java
@@ -0,0 +1,85 @@
+package org.misty.utils;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class Tracked {
+
+    public static Tracked create() {
+        return new Tracked(null, "", true);
+    }
+
+    public static Tracked create(String name) {
+        return new Tracked(null, name, true);
+    }
+
+    private final Optional<Tracked> parent;
+
+    private final String id;
+
+    private final String msg;
+
+    private Tracked(Tracked parent, String name, boolean joinIdentifier) {
+        this.parent = Optional.ofNullable(parent);
+
+        String id = parent == null ? "" : parent.id + "|";
+        id += name == null || name.isEmpty() ? "" : name + (joinIdentifier ? ":" : "");
+
+        if (joinIdentifier) {
+            String now = Long.toString(System.currentTimeMillis(), Character.MAX_RADIX);
+            String random = String.format("%04d", (int) (Math.random() * 1_0000));
+            this.id = id + now + random;
+        } else {
+            this.id = id;
+        }
+
+        this.msg = "TRACKED{" + this.id + '}';
+    }
+
+    public String say(String msg) {
+        return this.msg + " " + msg;
+    }
+
+    public Tracked link(String name) {
+        return link(name, false);
+    }
+
+    public Tracked linkWithRandom(String name) {
+        return link(name, true);
+    }
+
+    private Tracked link(String name, boolean joinIdentifier) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("name is null or empty");
+        }
+
+        return new Tracked(this, name, joinIdentifier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tracked)) return false;
+        Tracked tracked = (Tracked) o;
+        return Objects.equals(id, tracked.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return this.msg;
+    }
+
+    public Optional<Tracked> getParent() {
+        return parent;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
---
+++ src/test/java/org/misty/utils/TrackedTest.java
@@ -0,0 +1,50 @@
+package org.misty.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TrackedTest {
+
+    @Test
+    public void create_with_link() {
+        Tracked trackedA = Tracked.create("A");
+        Tracked trackedB = trackedA.link("B");
+        Tracked trackedC = trackedB.linkWithRandom("C");
+        System.out.println(trackedC);
+
+        Assertions.assertThat(trackedC.getParent().get()).isEqualTo(trackedB);
+        Assertions.assertThat(trackedB.getParent().get()).isEqualTo(trackedA);
+        Assertions.assertThat(trackedA.getParent()).isEmpty();
+    }
+
+    @Test
+    public void create_without_name() throws InterruptedException {
+        int createTimes = 5;
+        int eachRestMs = 100;
+
+        for (int i = 0; i < createTimes; i++) {
+            Tracked tracked = Tracked.create();
+            System.out.println(tracked);
+            Thread.sleep(eachRestMs);
+        }
+    }
+
+    @Test
+    public void create_with_name() throws InterruptedException {
+        int createTimes = 5;
+        int eachRestMs = 100;
+
+        for (int i = 0; i < createTimes; i++) {
+            Tracked tracked = Tracked.create("test");
+            System.out.println(tracked);
+            Thread.sleep(eachRestMs);
+        }
+    }
+
+    @Test
+    public void say() {
+        Tracked tracked = Tracked.create();
+        System.out.println(tracked.say("kerker"));
+    }
+
+}

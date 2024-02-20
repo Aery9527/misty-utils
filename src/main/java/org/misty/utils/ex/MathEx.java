91547ab73f3b84e9124f3deed374a6d676aba0c6
aerylin <aerylin@pactmail.com>
2024-02-20 18:04:09 +0800

    enhance MathEx

--- src/main/java/org/misty/utils/ex/MathEx.java
+++ src/main/java/org/misty/utils/ex/MathEx.java
@@ -1,5 +1,6 @@
 package org.misty.utils.ex;
 
+import java.math.BigDecimal;
 import java.util.stream.LongStream;
 
 public class MathEx {
@@ -17,6 +18,22 @@ public class MathEx {
 
     public static long getDecimalBit(long value) {
         return (int) Math.log10(value) + 1;
+    }
+
+    public static float normalizationPercentage(double value) {
+        return normalization(value * 100, 2);
+    }
+
+    public static float normalizationPercentage(double value, int scale) {
+        return normalization(value * 100, scale);
+    }
+
+    public static float normalization(double value) {
+        return normalization(value, 2);
+    }
+
+    public static float normalization(double value, int scale) {
+        return new BigDecimal(value).setScale(scale, BigDecimal.ROUND_HALF_UP).floatValue();
     }
 
 }
